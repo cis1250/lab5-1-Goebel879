f26442fb4b36a12eb271f93f7817875cea2d9959
Leo Goebel <goebell@uoguelph.ca>
2025-10-21 13:48:09 -0400

    Implement Fibonacci sequence generator with user input
    Added functions for user input and Fibonacci sequence generation.

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,32 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+def user_Input(terms): 
+    if(input_user1 > 0): 
+        return True 
+    else: 
+        print("Please enter a positive integer.")
+
+def gen_Fibonacci(terms):
+    forL = terms 
+    x1 = 0 
+    x2 = 1 
+    count = 0
+    for count in range(forL): 
+        print_Fibonacci(x1,x2,count) 
+        if(count%2 == 0):
+            x1+= x2 
+        else:
+            x2+= x1 
+        count += 1
+def print_Fibonacci(termA, termB,cycle): 
+    if(cycle%2 == 0): 
+        print(termA,end = " ") 
+    else: 
+        print(termB,end = " ") 
+input_user1 = 0 
+input_user = input("User input: ") 
+input_user1 = int(input_user) 
+x = user_Input(input_user1) 
+if(x == True): 
+    gen_Fibonacci(input_user1)
